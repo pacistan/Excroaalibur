f99d49a74b0a622e2434b1f1b975ee277594277b
Sneakysunset <andrelauzier@gmail.com>
2025-10-12 12:05:48 +0200

    Singleton and Empty GridManager Class

---
+++ Assets/_Projet/Scripts/GSingleton/GSingleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of " + GetType() + " found!");
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this as T;
+        }
+    }
+}
+
