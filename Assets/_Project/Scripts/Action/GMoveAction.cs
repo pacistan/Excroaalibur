02965a817f7402c0f66f1df9da884f87708af3c8
Ønym <onym.balz@gmail.com>
2025-10-13 17:13:30 +0200

    UI player controller connection

--- Assets/_Project/Scripts/Action/GMoveAction.cs
+++ Assets/_Project/Scripts/Action/GMoveAction.cs
@@ -56,4 +56,10 @@ public class GMoveAction : GAction
         //TODO Check valid path
         return true;
     }
+
+    public override bool IsValid()
+    {
+        //TODO Check valid path
+        return true;
+    }
 }
--- Assets/_Project/Scripts/Controller/GPlayerController.cs
+++ Assets/_Project/Scripts/Controller/GPlayerController.cs
@@ -28,7 +28,7 @@ public class GPlayerController : GController
         if (_selectedPlayer == newSelected) return;
         
         _selectedPlayer = newSelected;
-        selectedPlayerChanged?.Invoke(newSelected);
+        SelectedPlayerChanged?.Invoke(newSelected);
     }
 
     public void SetPlayerTurn()
