243cdd29764d8d48738728009e1990f3d474554e
Ønym <onym.balz@gmail.com>
2025-10-13 18:41:50 +0200

    follow path move

--- Assets/_Project/Scripts/Action/GMoveAction.cs
+++ Assets/_Project/Scripts/Action/GMoveAction.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class GMoveAction : GAction
 {
     private EHexDirection[] _path = new EHexDirection[] { };
-
+    private Vector3[] _wayPoints = new Vector3[] { };
     float _progress = 0;
     
     public override void PreProcess()
     {
         _path = GGridManager.Instance.GetPath(linkedPawn.currentCell ,targetCell, true);
         if (_path == null || _path.Length == 0) return;
+
+        GCell cell = linkedPawn.currentCell;
+        List<Vector3> wayPoints = new List<Vector3>();
+        wayPoints.Add(cell.transform.position);
+        foreach (var direction in _path)
+        {
+            cell = cell._neighbors[(int)direction];
+            if (cell == null) break;
+            wayPoints.Add(cell.transform.position);
+        }
+        _wayPoints = wayPoints.ToArray();
     }
 
     public override void Start_Action()
@@ -24,24 +36,19 @@ public class GMoveAction : GAction
     {
         base.Update_Action(delta);
         _progress += delta;
-        if (_progress >= 1)
+        if (_progress >= _wayPoints.Length - 1) 
         {
             End_Action();
             return;
         }
-        linkedPawn.transform.position = Vector3.Lerp(linkedPawn.transform.position, targetCell.transform.position, _progress);
+        int id = Mathf.FloorToInt(_progress);
+        linkedPawn.transform.position = Vector3.Lerp(_wayPoints[id], _wayPoints[id+1], Mathf.Repeat(_progress, 1));
     }
 
     public override void End_Action()
     {
         base.End_Action();
         linkedPawn.transform.position = targetCell.transform.position;
-    }
-
-    public override bool IsValid()
-    {
-        //TODO Check valid path
-        return true;
     }
 
     public override bool IsValid()
