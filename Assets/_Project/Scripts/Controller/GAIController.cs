f94ec7e2b787339323e23d1b1bfda725ccfc8274
Sneakysunset <andrelauzier@gmail.com>
2025-10-13 09:48:13 +0200

    Start On AI Controller

---
+++ Assets/_Project/Scripts/Controller/GAIController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(GPion))]
+public class GAIController : MonoBehaviour, GIController
+{
+    [SerializeField]
+    private int _startingActionTokensNumber;
+    private GPion _pawn;
+
+    public int actionTokens { get; set; }
+    
+    public void StartTurn()
+    {
+        actionTokens = _startingActionTokensNumber;
+        StartAction();
+    }
+
+    public void StartAction()
+    {
+        actionTokens--;
+    }
+
+    public void OnActionOver()
+    {
+        if (actionTokens == 0)
+        {
+            EndTurn();
+        }
+        else
+        {
+            StartAction();
+        }
+    }
+
+    public void EndTurn()
+    {
+    }
+    
+    void Start()
+    {
+        _pawn = GetComponent<GPion>();
+    }
+}
