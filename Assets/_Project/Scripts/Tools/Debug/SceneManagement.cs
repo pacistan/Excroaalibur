105fd331a3d636f3b21033260682e7f6fcae6620
Ønym <onym.balz@gmail.com>
2025-10-15 17:42:37 +0200

    Scene management

---
+++ Assets/_Project/Scripts/Tools/Debug/SceneManagement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneManagement : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI titleOfScene;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene(2);
+
+        }
+
+        if (titleOfScene)
+            titleOfScene.text = SceneManager.GetActiveScene().name;
+    }
+}
