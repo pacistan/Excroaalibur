77fd45f379612575307c7dc4a1b9c755c8009c34
Ønym <onym.balz@gmail.com>
2025-10-16 10:40:37 +0200

    Add Move out of hole action

---
+++ Assets/_Project/Scripts/Action/GMoveOutOfHoleAction.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMoveOutOfHoleAction : GAction
+{
+    public override void PreProcess()
+    {
+        if (!targetCell || targetCell.GetPawn() || targetCell.GetPawn() == linkedPawn) return;
+        //validate
+    }
+
+    public override void Start_Action()
+    {
+        base.Start_Action();
+        linkedPawn.SetCell(targetCell);
+        linkedPawn.transform.DOMove(targetCell.transform.position, .5f).SetEase(Ease.OutBack).onComplete = End_Action;
+    }
+
+    public override void Update_Action(float delta)
+    {
+        base.Update_Action(delta);
+    }
+
+    public override void End_Action()
+    {
+        base.End_Action();
+    }
+
+    public override GHexCoordinate[] GetValidCells()
+    {
+        if (linkedPawn.currentCell.GetTileType != ETileType.Hole)
+            return validCells = new GHexCoordinate[]{};
+        
+        List<GHexCoordinate> newValidCells = new List<GHexCoordinate>();
+
+        foreach (var cell in linkedPawn.currentCell._neighbors)
+        {
+            if (!cell || !cell.IsWalkable()) continue;
+            
+            newValidCells.Add(cell._hexCoordinates);
+        }
+        
+        return validCells = newValidCells.ToArray();
+    }
+}
