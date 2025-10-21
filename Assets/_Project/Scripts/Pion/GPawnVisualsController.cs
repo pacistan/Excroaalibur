eedd86071aa61212a302dfd97f3a9c8cdabbc0b8
Sneakysunset <andrelauzier@gmail.com>
2025-10-21 19:44:07 +0200

    Fix wrong Values on push + fix throw on adjacent target

--- Assets/_Project/Scripts/Pion/GPawnVisualsController.cs
+++ Assets/_Project/Scripts/Pion/GPawnVisualsController.cs
@@ -39,8 +39,10 @@ public class GPawnVisualsController : SerializedMonoBehaviour
 
     void Start()
     {
-        OnUpdateStunTurn();
-        OnUpdateHealthPoints();
+        if (!_pawn.isPlayer)
+        {
+            _debugTxt.text = $"{_pawn.hp}";
+        }
         _stunMaterialIndex = Mathf.Min(_mainRenderer.materials.Length, _stunMaterialIndex);
         _defaultMaterial = _mainRenderer.materials[_stunMaterialIndex];
     }
