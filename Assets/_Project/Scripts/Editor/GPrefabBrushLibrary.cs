967fd76026a33e95bd23697a356fe2f1de882f37
Sneakysunset <andrelauzier@gmail.com>
2025-10-22 17:43:24 +0200

    PrefabPainter Update for serialization

--- Assets/_Project/Scripts/Editor/GPrefabBrushLibrary.cs
+++ Assets/_Project/Scripts/Editor/GPrefabBrushLibrary.cs
@@ -1,34 +1,15 @@
 ﻿using Sirenix.OdinInspector;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "PrefabBrushLibrary", menuName = "Painter Tool/Prefab Brush Library")]
 public class GPrefabBrushLibrary : SerializedScriptableObject
 {
-    [System.Serializable]
-    public class BrushData
-    {
-        [FoldoutGroup("$brushName")]
-        public string brushName = "New Brush";
-        [FoldoutGroup("$brushName")]
-        public GameObject prefab;
-        [FoldoutGroup("$brushName")]
-        public Texture2D icon;
-        
-        [Header("Placement Settings")]
-        [FoldoutGroup("$brushName")]
-        public bool randomRotation = false;
-        [FormerlySerializedAs("incrementalRotation")]
-        [FoldoutGroup("$brushName"), ShowIf("randomRotation")]
-        public bool isIncrementalRotation = false;
-        [FoldoutGroup("$brushName"),HideIf("randomRotation")]
-        public float rotationOffset = 0;
-    }
+
     
     [Header("Brush Collection")]
     [SerializeField]
-    public List<BrushData> brushes = new List<BrushData>();
+    public List<GBrushData> brushes = new List<GBrushData>();
     
     [Header("Default Settings")]
     public float defaultBrushSize = 1f;
