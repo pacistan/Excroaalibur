1c34a6f61244d9c551eddc23062149a15882524b
Ønym <onym.balz@gmail.com>
2025-10-24 14:14:31 +0200

    Music integration & setting menu

--- Assets/_Project/Scripts/Manager/GameManager.cs
+++ Assets/_Project/Scripts/Manager/GameManager.cs
@@ -1,15 +1,75 @@
-﻿using UnityEngine;
+﻿using FMOD.Studio;
+using FMODUnity;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 /* Responsable de la gestion globale du Jeu, de l'activation de potentiel Manager etc...*/
 public class GameManager: GSingleton<GameManager>
 {
     public bool isGamePaused = false;
+
+    InputAction _menuInput;
+    
+    [SerializeField]
+    PauseMenu _menu;
+    
+    [SerializeField]
+    EventReference musicEvent;
+    EventInstance musicInstance;
+    EventInstance pauseSnapshot;
     
     public void PauseGame(bool resume = false)
     {
         isGamePaused = !resume;
+        if (resume)
+            pauseSnapshot.stop(STOP_MODE.ALLOWFADEOUT);
+        else
+            pauseSnapshot.start();
         // TODO : Potentially add more logic here (e.g., notify other systems)
     }
-    
+
+    void StartMusic()
+    {
+        if (musicEvent.IsNull) return;
+        pauseSnapshot = RuntimeManager.CreateInstance("snapshot:/Pause");
+        musicInstance = RuntimeManager.CreateInstance(musicEvent);
+        musicInstance.start();
+    }
+
+    void OpenMenu()
+    {
+        if (!_menu) return;
+        PauseGame();
+        
+        _menu.Open();
+    }
+
+    void CloseMenu()
+    {
+        if (!_menu) return;
+        PauseGame(true);
+        _menu.Close();
+    }
+
+    void Start()
+    {
+        _menuInput = InputSystem.actions.FindAction("Menu");
+        StartMusic();
+    }
+
+    void Update()
+    {
+        if (_menuInput.WasPressedThisFrame())
+        {
+            if (_menu.IsOpen)
+                CloseMenu();
+            else
+                OpenMenu();
+        }
+    }
+
     // TODO : Manage the Scene transitions, loading screens, etc.
 }
---
+++ Assets/_Project/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenu : MonoBehaviour
+{
+    CanvasGroup _canvasGroup;
+
+    public bool IsOpen =>  _canvasGroup.interactable;
+    
+    public void Open()
+    {
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+        _canvasGroup.DOFade(1, .5f).SetEase(Ease.OutCirc);
+    }
+
+    public void Close()
+    {
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+        _canvasGroup.DOFade(0, .5f).SetEase(Ease.OutCirc);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Awake()
+    {
+        _canvasGroup = gameObject.GetComponent<CanvasGroup>();
+    }
+}
