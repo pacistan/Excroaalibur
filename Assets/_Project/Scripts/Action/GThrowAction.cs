d88cbbfda2ef64078a78192efa7c1bd4e58c88d4
Stan <99252047+Stanpac@users.noreply.github.com>
2025-10-26 18:24:29 +0100

    Highlight Case good

--- Assets/_Project/Scripts/Action/GThrowAction.cs
+++ Assets/_Project/Scripts/Action/GThrowAction.cs
@@ -89,7 +89,6 @@ public class GThrowAction : GAction
         {
             GAction impactReaction = _targetPawn.GetReaction(this);
             previsuContext.Set(GActionContext.DAMAGE_STRING, _crown._currentDamage);
-
             if (impactReaction != null)
             {
                 previsuContext.Set(GActionContext.DIRECTION_STRING, direction);
