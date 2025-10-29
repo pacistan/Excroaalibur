13d50acfed95daecca2936ae628b54311d99cfbe
Stan <99252047+Stanpac@users.noreply.github.com>
2025-10-29 16:19:24 +0100

    Selected and Unselected action

--- Assets/_Project/Scripts/Controller/GPlayerController.cs
+++ Assets/_Project/Scripts/Controller/GPlayerController.cs
@@ -77,7 +77,6 @@ public class GPlayerController : GController
             _selectedAction.OnUnselectedAction();
         
         GPawn targetPawn = _hoverCell ? _hoverCell.GetGridObject<GPawn>() : null;
-
         _selectedAction = action;
         
         int index = action != null && action.GetType() != typeof(GThrowAction) ? 0 :
