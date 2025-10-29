1db3764e20a522c80d352a2d3d2adde05aaaea91
Stan <99252047+Stanpac@users.noreply.github.com>
2025-10-29 16:19:24 +0100

    Selected and Unselected action

--- Assets/_Project/Scripts/Controller/GPlayerController.cs
+++ Assets/_Project/Scripts/Controller/GPlayerController.cs
@@ -69,7 +69,6 @@ public class GPlayerController : GController
         
         if (_selectedAction != null) 
             _selectedAction.OnUnselectedAction();
-        
         _selectedAction = action;
         
         if (_selectedAction == null) return;
