8d14acfd550825498f4ee71c4e1cafa8c3ccc753
Stan <99252047+Stanpac@users.noreply.github.com>
2025-10-30 11:51:47 +0100

    Add TileType for Pawn + Start Utilities

--- Assets/_Project/Scripts/Controller/GPlayerController.cs
+++ Assets/_Project/Scripts/Controller/GPlayerController.cs
@@ -72,6 +72,7 @@ public class GPlayerController : GController
         
         if (_selectedAction != null) 
             _selectedAction.OnUnselectedAction();
+        
         _selectedAction = action;
         
         if (_selectedAction == null) return;
