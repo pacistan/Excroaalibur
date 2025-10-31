547171905cb84128cd97c490e089701947da43aa
Stan <99252047+Stanpac@users.noreply.github.com>
2025-11-01 00:15:21 +0100

    Tutorial Code Good

--- Assets/_Project/Tutorial/GTutorialHandler.cs
+++ Assets/_Project/Tutorial/GTutorialHandler.cs
@@ -68,6 +68,7 @@ public class GTutorialHandler : MonoBehaviour
     
     IEnumerator WaitAndHideInfo(STutorialInfo info)
     {
+
         GGameManager.Instance.playerController.enabled = false; 
         yield return new WaitForSeconds(info.stopTime);
         
