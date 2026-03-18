bb3b80d0c7fd3d0667ba3db3b4c0e49029113d31
Ønym <onym.nightly@proton.me>
2026-03-18 19:25:25 +0100

    Upgrade screen (#195)
    * Tweak ui fixes
    
    * Tweak map select animation
    
    * Use prefab in main scene
    
    * Upgrade UI
    
    * Tweak upgrade desc background size
    
    * Tweak upgrade select animation
    
    ---------
    
    Co-authored-by: Ønym <onym.balz@gmail.com>

--- Assets/_Project/Scripts/Maps/GMapCard.cs
+++ Assets/_Project/Scripts/Maps/GMapCard.cs
@@ -14,6 +14,9 @@ public class GMapCard : MonoBehaviour
     Button _button;
 
     [SerializeField]
+    LocalizeStringEvent _nameTxt;
+        
+    [SerializeField]
     Image _mapCoverImage;
     
     [SerializeField]
@@ -27,7 +30,7 @@ public class GMapCard : MonoBehaviour
 
     [SerializeField]
     Image _progressFillBar;
-    
+
     [SerializeField]
     LocalizeStringEvent _conditionMapTxt;
 
@@ -53,6 +56,8 @@ public class GMapCard : MonoBehaviour
     {
         bool isMapUnlocked = IsMapUnlocked();
         _lockImage.enabled = !isMapUnlocked;
+        _nameTxt.StringReference = mapData.MapName;
+        _nameTxt.RefreshString();
         if (mapData.ProgressMapToUnlock)
         {
             _progressTxt.text = $"{mapData.ProgressMapToUnlock.NumberOfWavesOnThisMap.ToString()} / {mapData.NumberOfWavesToUnlock.ToString()}";
@@ -69,7 +74,7 @@ public class GMapCard : MonoBehaviour
     {
         if (_clickSeq.IsActive() && _clickSeq.IsPlaying()) return;
         _SelectorImage.gameObject.SetActive(true);
-       _selectorLoop.Play();
+       _selectorLoop.Restart();
     }
     
     public void OnUnselected()
--- Assets/_Project/Scripts/UI/GUpgradeCardSelectMenu.cs
+++ Assets/_Project/Scripts/UI/GUpgradeCardSelectMenu.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
 using Sirenix.Utilities;
 using System;
 using System.Collections.Generic;
@@ -9,12 +10,18 @@ public class GUpgradeCardSelectMenu : MonoBehaviour
     [SerializeField]
     List<GUpgradeCard> _upgrades = new List<GUpgradeCard>();
 
+    CanvasGroup _canvasGroup;
+    
+    Sequence _showSequence;
+    
     void Start()
     {
         for (var i = 0; i < _upgrades.Count; i++)
         {
             _upgrades[i].Initialize(i);
         }
+        
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void BuildUpgradesUI(ref List<GSOUpgrade> upgradesData)
@@ -23,5 +30,30 @@ public class GUpgradeCardSelectMenu : MonoBehaviour
         {
             _upgrades[i].Build(upgradesData[i]);
         }
-    }    
+    }
+
+    public void ShowScreen()
+    {
+        if (_showSequence.IsActive()) _showSequence.Kill();
+        _showSequence = DOTween.Sequence();
+        
+        _showSequence.Append(_canvasGroup.DOFade(1f, .5f).From(0f).SetEase(Ease.OutSine));
+        _showSequence.AppendInterval(.15f);
+        foreach (var upgrade in _upgrades)
+        {
+            _showSequence.JoinCallback(upgrade.Show);
+        }
+    }
+
+    public void HideScreen()
+    {
+        if (_showSequence.IsActive()) _showSequence.Kill();
+        _showSequence = DOTween.Sequence();
+        
+        foreach (var upgrade in _upgrades)
+        {
+            _showSequence.JoinCallback(upgrade.Hide);
+        }
+        _showSequence.Append(_canvasGroup.DOFade(0f, .5f).SetEase(Ease.OutSine));
+    }
 }
